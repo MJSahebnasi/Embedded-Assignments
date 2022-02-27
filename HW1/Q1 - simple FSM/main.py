a52cf7e7fb37df9e6ebaa3e3c727e298c674a70b
MJSahebnasi <mjsahebnasi@gmail.com>
2022-02-27 12:50:02 +0330

    base code

--- HW1/Q1 - simple FSM/main.py
+++ HW1/Q1 - simple FSM/main.py
@@ -1,4 +1,7 @@
 import enum
+import threading
+import time
+
 import matplotlib.pyplot as plt
 
 
@@ -7,9 +10,10 @@ class State(enum.Enum):
     S2 = 2
     S3 = 3
 
+
 heater = False
 cooler = False
-round_per_sec = 0
+cooler_round_per_sec = 0
 
 crnt_state = State.S1
 next_state = None
@@ -17,60 +21,58 @@ next_state = None
 supernode_crnt_state = None  # todo
 supernode_next_state = None
 
-time = 0
+T_lock = None  # for both T and T_modified
 T = 25  # temperature
+T_modified = False
 
-# each of these values will be assigned to <T> at the corresponding time
-T_manual_change_values = [13, 28, 40, 26, 12, 32, 14, 40, 42]
-T_manual_change_times = [3, 8, 13, 19, 24, 30, 36, 41, 47]
-
-# T values will be stored here
-T_history = []
+terminate_program = False
 
 
-def air_conditioner(T):
-    match crnt_state:
-        case
-    return T
+class AirConditioner(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while not terminate_program:
+            if crnt_state == State.S1:
+                # todo
+                return
+            elif crnt_state == State.S2:
+                return
+            elif crnt_state == State.S3:
+                return
+        return T
 
 
-def draw_chart(T_history):
-    # TODO
-    fig, ax = plt.subplots()
-    ax.scatter(range(len(T_history)), T_history)
-    # plt.plot(range(len(T_history)), T_history, '--o')
-    # ax.annotate('txt', (1, T_history[1]))
-    ax.annotate("X",
-                xy=(1, T_history[1]), xycoords='data',
-                xytext=(1, T_history[1] + 3), textcoords='data',
-                arrowprops=dict(arrowstyle="->", color="0.5",
-                                shrinkA=5, shrinkB=5,
-                                patchA=None, patchB=None,
-                                connectionstyle="arc3,rad=0.",
-                                ),
-                )
-    plt.show()
+class UserInput(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global terminate_program
+        global T
+        global T_modified
+
+        while not terminate_program:
+            time.sleep(2)
+            user_input = int(input(' -> enter an int as a new value for T (-1 to terminate the program): '))
+            print(user_input)
+            if user_input == -1:
+                terminate_program = True
+            else:
+                T_lock.acquire()
+                T = user_input
+                T_modified = True
+                T_lock.release()
 
 
 if __name__ == '__main__':
-    global time
-    max_time = 50
+    print('the initial T: ' + str(T))
 
-    
+    T_lock = threading.Lock()
 
-# for time in range(max_time + 1):
-#
-#     if time in T_manual_change_times:
-#         # applying manual changes to test the air_conditioner:
-#         T = T_manual_change_values[T_manual_change_times.index(time)]
-#     else:
-#         T = air_conditioner(T)
-#         # I've separated these parts so that the changes would be vividly visible
-#
-#     T_history.append(T)
+    user_input_thread = UserInput()
+    air_conditioner_thread = AirConditioner()
 
-# draw_chart(T_history)
-
-## draw_chart(T_manual_changes)
-
-
+    user_input_thread.start()
+    air_conditioner_thread.start()
